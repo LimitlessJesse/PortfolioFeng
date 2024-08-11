6a02f2c8fa0e0af83db2265b42304f8745c475e1
LimitlessJesse <jessewu1999@gmail.com>
2024-08-11 18:51:39 -0500

    Finish base project

---
+++ app/(home)/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar(){
+
+  return (
+    <nav className=" py-10 flex justify-between items-center">
+      <h1 className="text-6xl font-bold underline underline-offset-8 decoration-purple-400">Lil Feng</h1>
+    </nav>
+  );
+}
---
+++ app/(home)/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React from "react";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import { AuroraBackground } from "./components/ui/aurora-backgroud";
+import { motion } from "framer-motion";
+import Links from "./components/Links";
+
+export default function page(){
+  return(
+    <AuroraBackground>
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="relative flex flex-col gap-4 justify-center px-4"
+      >
+        <div className="min-h-screen">
+          <div className="max-w-7xl mx-auto">
+            
+            <Navbar />
+            <HeroSection />
+            
+          </div>
+          <div className="max-w-7xl mx-auto items-center mt-20"> 
+            <Links />
+          </div>
+        </div>
+      </motion.div>
+    </AuroraBackground>
+
+  );
+}
