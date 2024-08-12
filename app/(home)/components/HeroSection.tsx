e033447e04ff5d4a882fd43727f30b2bcb4f9abc
LimitlessJesse <jessewu1999@gmail.com>
2024-08-12 15:53:42 -0500

    added description

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -2,6 +2,8 @@ import React from "react"
 
 export default function HeroSection() {
   return (
-    <div> Description Here </div>
+    <div>
+      Lil Feng, Suzhou, China born and raised. Came to Canada at age 15, started involved with Hip-hop culture in Toronto, and became a Chinese rapper and producer. After moving to Winnipeg, he joined a local Chinese Hip-hop label <q>Smoking House</q> in 2022.  He produced his first Hip-hop album <q>Night Elf</q> in 2023. Meanwhile, he worked in a machining factory for three years to save up his studio and began recording music on his own. In 2023, the <q>Smoking House</q> label was disbanded. Lil Feng created his own music label <q>Overload Music</q>. He got into many local performances, <q>Rap of China </q> 2024 in Toronto, and some music video shoots. Now the story is still rolling, Stay tuned for more legendary moments to be made.  - Lil Feng 
+    </div>
   )
 }
