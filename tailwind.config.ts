4561c2016d2db59e1b49fb9bf71cbe60901cc32d
LimitlessJesse <jessewu1999@gmail.com>
2024-08-11 21:22:55 -0500

    Test fix performance

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -75,14 +75,14 @@ const config = {
             backgroundPosition: "50% 50%, 50% 50%",
           },
           to: {
-            backgroundPosition: "350% 50%, 350% 50%",
+            backgroundPosition: "350% 350%, 350% 350%",
           },
         },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        aurora: " 60s linear infinite",
+        aurora: "aurora 60s linear infinite",
       },
     },
   },
