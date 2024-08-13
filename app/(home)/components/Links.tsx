f3b4ff788d7beae3831c13f8bb0422701be5ac75
LimitlessJesse <jessewu1999@gmail.com>
2024-08-12 22:59:11 -0500

    change theme to green

--- app/(home)/components/Links.tsx
+++ app/(home)/components/Links.tsx
@@ -38,7 +38,7 @@ export default function Links(){
 
   return (
     <nav className=" flex flex-col items-center mt-5">
-      <h1 className="text-4xl font-bold underline underline-offset-8 decoration-purple-300">My Links</h1>
+      <h1 className="text-4xl font-bold underline underline-offset-8 decoration-green-500">My Links</h1>
       <div className="">
         <HoverEffect items={links} />
       </div>
